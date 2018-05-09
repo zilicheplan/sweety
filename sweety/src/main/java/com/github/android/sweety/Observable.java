8d452ab791142486b61adb591923cf98e4711575
Eddie123 <806434283@qq.com>
2018-05-10 00:55:30 +0800

    sweety

---
+++ sweety/src/main/java/com/github/android/sweety/Observable.java
@@ -0,0 +1,9 @@
+package com.github.android.sweety;
+
+/**
+ * Created by eddie on 2018/5/10.
+ */
+
+public interface Observable {
+
+}
---
+++ sweety/src/main/java/com/github/android/sweety/Updateable.java
@@ -0,0 +1,10 @@
+package com.github.android.sweety;
+
+/**
+ * Created by eddie on 2018/5/10.
+ */
+
+public interface Updateable {
+
+    void update();
+}
